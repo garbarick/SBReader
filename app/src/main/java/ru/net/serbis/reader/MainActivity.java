27dbea12bd11a0d15275955ce1a16399a003f33b
garbarick <serbis@gmail.com>
2017-09-29 15:23:10 +0400

    Initial commit

---
+++ app/src/main/java/ru/net/serbis/reader/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.net.serbis.reader;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
